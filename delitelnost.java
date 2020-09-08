e4d53921255cf46efd5293d92d9a82393d652ed2
BB0ddY <62869261+BB0ddY@users.noreply.github.com>
2020-09-08 22:12:58 +0200

    Add files via upload

---
+++ delitelnost.java
@@ -0,0 +1,10 @@
+public class delitelnost {
+    	public static void main(String args[]) {
+		System.out.println("Delitelne 7 jsou: ");		
+		for (int i=1; i<100; i++) {
+			if (i%7==0) 
+			System.out.print(i +", ");			
+		}			
+		
+  }
+}
